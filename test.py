a905809108723774a1ce0da1edbd896312d51ca3
Zoe Jane <dadac123@gmail.com>
2018-04-01 20:50:40 +0800

    add(comments) for learning read commit part

--- test.py
+++ test.py
@@ -2,17 +2,36 @@ from github import Github
 import datetime
 import config
 
+# login github and get the github object
+# learn more about Github Objects in PyGithub
+# http://pygithub.readthedocs.io/en/latest/github_objects.html
 github_obj = Github(config.access_token)
 
-repo_name = input("input repo name: ")
+# get repo_name from user
 
+# repo_name = input("input repo name: ")
+repo_name = "pygithub-test"
+
+# get the Repository Object matches the user's input
+# learn more about Repository in PyGithub
+# http://pygithub.readthedocs.io/en/latest/github_objects/Repository.html
+
+# get_user()
+# get_repo()
+# learn more about Main class: Github in PyGithbb
+# http://pygithub.readthedocs.io/en/latest/github.html
 repo_obj = github_obj.get_user().get_repo(repo_name)
 
-branch_name = input("branch_name: ")
+# branch_name = input("branch_name: ")
+branch_name = "master"
 
-commits = repo_obj.get_commits(sha = branch_name, since = datetime.datetime.now() - datetime.timedelta(days = 7), until = datetime.datetime.now())
+# learn more about get_commits()
+# http://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository.get_commits
+# learn more about timedelta
+# https://docs.python.org/3/library/datetime.html#timedelta-objects
+all_commits = repo_obj.get_commits(sha = branch_name, since = datetime.datetime.now() - datetime.timedelta(days = 7), until = datetime.datetime.now())
 
-for cm in commits:
-	print(cm.sha)
-	print(cm.commit.message)
+for a_commit in all_commits:
+	print(a_commit.sha)
+	print(a_commit.commit.message)
 	print("\n")
